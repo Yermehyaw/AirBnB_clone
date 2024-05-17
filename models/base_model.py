bf665fea7fb4085f5a55e80539b272be60f8f665
Yermehyaw <yermehyaw@gmail.com>
2024-05-17 22:31:58 +0100

    Add: Better logic in its __init__method of the BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -31,11 +31,10 @@ class BaseModel():
         kwargs (dict): Variable number of keyword arguments
 
         """
-        if kwargs == {} or kwargs is None:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-        else:  # only the available variables should be used, if not a variable is not available the standard values shoukd be initialized
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs != {} and kwargs is not None:
             for attr, value in kwargs.items():
                 if attr == "id":
                     self.id = value
