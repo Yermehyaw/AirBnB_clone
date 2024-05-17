9d698a7f1fed287f7ce3fcee16a8f7968858c89f
Yermehyaw <yermehyaw@gmail.com>
2024-05-17 12:33:16 +0100

    Add: inline comment

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-        else:
+        else:  # only the available variables should be used, if not a variable is not available the standard values shoukd be initialized
             for attr, value in kwargs.items():
                 if attr == "id":
                     self.id = value
