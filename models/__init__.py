a83625f89e7104b856d5d301acbdac3801993d17
Yermehyaw <yermehyaw@gmail.com>
2024-05-18 20:15:40 +0100

    Add: package constructor for the models package

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Impoted Modules: file_storage
+
+file_storage
+serializes instances to a JSON file and deserializes JSON file to instances
+
+"""
+import engines.file_storage  ###
+
+
+# Creates a unique FileStorage class object
+storage = file_storage.FileStorage()
+storage.reload()
 
