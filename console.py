c0eb107db13215521d046b9a2dd4016c1d8a4392
Yermehyaw <yermehyaw@gmail.com>
2024-05-22 13:07:13 +0100

    Add do_ methods for create and show commands

--- console.py
+++ console.py
@@ -4,10 +4,13 @@
 Modules Imported: cmd
 
 cmd: contains classes for creating a python command interpreter
+BaseMOdel: base class for all hbnb classes
+FileStorage: class defining methods used to store objects in JSON
 
 """
 import cmd
-
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class HBNBCommand(cmd.Cmd):
     """Entry point of the command interpreter for the Airbnb clone
@@ -24,10 +27,10 @@ class HBNBCommand(cmd.Cmd):
         """Exit the command interpreter
 
         Args:
-        None
+        line (str): Arguments passed to the command
 
         """
-        if line != "":
+        if line != "":  # do_quit() should receive no argument(s)
             print("Usage: quit")
             return
         else:
@@ -46,7 +49,7 @@ class HBNBCommand(cmd.Cmd):
         """Exit the command interpreter
 
         Args:
-        None
+        line (str): Arguments passed to command
 
         """
         if line != "":
@@ -71,6 +74,83 @@ class HBNBCommand(cmd.Cmd):
 
         """
         pass  # do nothing
+
+
+    def do_create(self, line=""):
+        """Creates a new instance of the BaseMOdel class
+
+        Args:
+        line (str): Arguments passed to command
+
+        """
+        if line == "":
+            print("** class name missing **")
+            return
+        list_args = line.split()
+        no_args = len(list_args)
+        if no_args > 1:  # can be removed to allow multiple object creation
+            print("** Enter only one class name **")
+            return
+        if line == "BaseMOdel":
+            new_obj = BaseMOdel()
+        FileStorage.new(new_obj)
+        FileStorage.save()
+        print(new_obj.id)
+    # extra valid classes are added here
+    else:
+        print("** class doesn't exist **")
+
+    def help_create(self):
+        """Help docs for do_create()
+
+        Args:
+        None
+
+        """
+        print("Creates a new instance of BaseModel and saves it")
+    
+
+    def do_show(self, line=""):
+        """Prints the string rep of an instance
+
+        Args:
+        line (str): Arguments passed to command
+
+        """
+        if line == "":
+            print("** class name missing **")
+            return
+        list_args = line.split()
+        no_args = len(list_args)
+        if no_args == 0:  # just in case ;)
+            print("** class name missing **")
+        elif no_args == 1:
+            print("** instance id missing **")
+            return
+
+        if list_args[0] == "BaseModel":
+            key = f"{list_args[0]}.{list_args[1]}"
+            FileStorage.reload()
+            all_objects = Filetorage.all()
+            try:
+                object_found = all_objects[key]
+                print(object_found)
+            except (KeyError):
+                print("** no instance found **")
+        # any other valid class names can be aded here best in a match statement
+        else:
+            print ("** class doesn't exist **")
+
+    def help_show(self):
+        """Help docs for do_show()
+
+        Args:
+        None
+
+        """
+        print("Prints the string representation of an instance based
+                on the class name and id passed\nUsage: <class> <class_id>")
+
 
 
 if __name__ == "__main__":
