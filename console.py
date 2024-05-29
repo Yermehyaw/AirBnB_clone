b458c0ec14ddbe7ae2f150ac6d91d80e23faced2
Yermehyaw <yermehyaw@gmail.com>
2024-05-29 07:36:35 +0100

    Add destroy and update commnads to console

--- console.py
+++ console.py
@@ -107,8 +107,7 @@ class HBNBCommand(cmd.Cmd):
         None
 
         """
-        print("Creates a new instance of BaseModel and saves it")
-    
+        print("Creates a new instance of BaseModel and saves it")    
 
     def do_show(self, line=""):
         """Prints the string rep of an instance
@@ -137,10 +136,104 @@ class HBNBCommand(cmd.Cmd):
                 print(object_found)
             except (KeyError):
                 print("** no instance found **")
-        # any other valid class names can be aded here best in a match statement
+        # any other valid class names can be aded here(best in a match statement)
         else:
             print ("** class doesn't exist **")
 
+    def do_destroy(self, line=""):
+        """Deletes an instance of a specified class
+
+        Args:
+        line (str): Arguments passed to command
+
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+            return
+        list_args = line.split()
+        no_args = len(list_args)
+        if no_args == 0:  # just in case ;)
+            print("** class name missing **")
+        elif no_args == 1:
+            print("** instance id missing **")
+            return
+
+        if list_args[0] == "BaseModel":
+            key = f"{list_args[0]}.{list_args[1]}"
+            FileStorage.reload()
+            all_objects = FileStorage.all()
+            try:
+                all_objects.remove(key)
+                FileStorage.objects = all_objects
+                FileStorage.save()
+            except Exception:
+                print("** no instance found **")
+        # other valid instance classes
+    else:
+        print("** instance id missing **")
+
+    def do_all(self, line=""):
+        """Print string representations of all instances
+
+        Args:
+        line (str): Argument(s) passed to command
+
+        """
+        if line is None:  # what happens when None is passed to a method expecting zero args
+            print("** class doesn't exist **")
+            return
+        else:
+            list_args = line.split()
+            no_args = len(list_args)
+            if no_args == 0:  # if no argument is passed, still print all instances
+                FileStorage.all()
+                return
+            for class_name in list_args:
+                if class_name == "BaseModel":
+                    try:
+                        #FileStorage.all()
+                # other valid class names
+                else:
+                    print ("** class doesn't exist **")
+
+    def do_update(self, line-""):
+        """Updates an instance of a valid class
+
+        Args:
+        line (str): Argument(s) passed to command
+        
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+            return
+        else:
+            list_args = line.split()
+            no_args = len(list_args)
+            # Verify the correct no of args are passed
+            if no_args == 0:
+                print("** class name missing **")
+                return
+            elif no_args == 1:
+                print("** instance id missing **")
+                return
+            elif no_args == 2:
+                print("** attribute name missing **")
+                return
+            elif no_args == 3:
+                print("** value missing **")
+                return
+            else: # the correct no of args is 4, anything else > 4 is ignored
+                class_name = list_args[0]
+                instance_id = list_args[1]
+                attr_name = list_args[2]
+                attr_value = list_args[3]
+                # Verify a valid class is entered
+                if class_name == "BaseModel":
+
+
+
+
+# Help methods for commands
     def help_show(self):
         """Help docs for do_show()
 
@@ -151,6 +244,15 @@ class HBNBCommand(cmd.Cmd):
         print("Prints the string representation of an instance based
                 on the class name and id passed\nUsage: <class> <class_id>")
 
+    def help_destroy(self):
+        """Help docs for do_destroy()
+
+        Args:
+        None
+        
+        """
+        print("Deletes an instance of a class\nUsage: destroy <class-name> 
+                <instance-id>")
 
 
 if __name__ == "__main__":
